23793713ab87b6101f67dc70b731d4084503ef40
toannguyenle <toan@me.com>
2014-10-30 11:25:21 -0700

    First Commit for Heroku deployment

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User
+  include Mongoid::Document
+  include ActiveModel::SecurePassword
+  has_secure_password
+  #brief time in RAM only a true password.
+  # Hidden field
+  # password
+  # password_confirmation
+  field :email, type: String
+  field :password_digest, type: String #hash to store encrypted password
+end
